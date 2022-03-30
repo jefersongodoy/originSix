cb52a5beae812e7dcfcb2b7a38fe5e106c2b445d
Godoy <j.godoysoares@gmail.com>
2022-03-30 13:37:03 -0300

    depoimentos e contato

--- script/main.js
+++ script/main.js
@@ -1,30 +1,40 @@
 /* abre e fecha o menu quando clicar no icone: hamburguer e x */
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle) {
-    element.addEventListener('click', () =>  {
-        nav.classList.toggle('show');
-    })
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
 }
 
 /* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links) {
-    link.addEventListener('click', () => {
-        nav.classList.remove('show')
-    })
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
 }
 
 /*mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= navHeight) {
-        header.classList.add('scroll')
-    }else {
-        header.classList.remove('scroll')
-    }
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
 })
